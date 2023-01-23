48b101c66f8e16bb68fec74ee8f04bf3a03310ad
rithika1462 <rithika1462@gmail.com>
2023-01-23 18:56:52 +0530

    rithika

--- src/redpackege/Blueclass.java
+++ src/redpackege/Blueclass.java
@@ -3,6 +3,7 @@ package redpackege;
 public class Blueclass {
 
 	public static void main(String[] args) {
+		System.out.println("black");
 		System.out.println("white");
 	System.out.println("yellow");
 
