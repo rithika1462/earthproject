4bf38adc1f274227a5161b9152ddbb66ad73b357
rithika1462 <rithika1462@gmail.com>
2023-01-23 18:52:34 +0530

    dolly

--- src/redpackege/Blueclass.java
+++ src/redpackege/Blueclass.java
@@ -3,7 +3,7 @@ package redpackege;
 public class Blueclass {
 
 	public static void main(String[] args) {
-		System.out.println("red");
+		System.out.println("white");
 	System.out.println("yellow");
 
 	}
